28a7e014f65a5dbc471b0f8615ff5defdfe37c1a
Unknown <nicobighetti@gmail.com>
2018-02-04 19:19:00 +0100

    add fex example files
    code might not work

--- TUIOSamples/src/DemoState.js
+++ TUIOSamples/src/DemoState.js
@@ -1,22 +1,32 @@
 var FastTable = {
-	_WIDTH: 480,
-	_HEIGHT: 320
+	_WIDTH: 1920,
+	_HEIGHT: 1080
 };
 FastTable.DemoState = function(game) {
   this.game = game;
+	this._roomHeight = 250;
+	this._roomWidth = 250;
+
 };
 FastTable.DemoState.prototype = {
 	preload: function() {
     this.game.load.image('background', './src/img/splash_background.jpg');
     this.game.load.image('star_layer', './src/img/splash_parallax_layer.png');
     this.game.load.image('star_layer_2', './src/img/splash_parallax_layer_2.png');
+
+		this.game.load.image('room', './src/img/room.png');
   },
 	create: function() {
-		/*this.game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
-		this.game.state.start('SplashScreen', true, false, MINIGAMELIST.FAST_GAME_SWITCH);*/
     this.background = this.game.add.tileSprite(0, 0, 1920, 1080, 'background');
     this.mediumLayer = this.game.add.tileSprite(0, 0, 1920, 1080, 'star_layer');
     this.frontLayer = this.game.add.tileSprite(0, 0, 1920, 1080, 'star_layer_2');
+
+		var nbRoom = 5;
+		var maxHeight = ;
+		var maxWidth = ;
+
+
+
 	},
   update: function(){
     this.background.tilePosition.x += 0.1;
