abc7b921fa7e4f983aa71c962d502b486f850877
Flavian Jacquot <flavian.jacquot@gmail.com>
2018-02-08 23:11:42 +0100

    add game reference to ship widget

--- TUIOSamples/src/ImageWidget/roomWidget.js
+++ TUIOSamples/src/ImageWidget/roomWidget.js
@@ -12,26 +12,29 @@ class RoomWidget extends TUIOWidget {
     this._lastTagsValues = {};
     this.eventsList = [];
     this.dors = [];
-
-    this._domElem = $('<img>');
+    console.log('ROOM WIDGET CREATED');
+/*    this._domElem = $('<img>');
 //    this._domElem.attr('src', imgSrc);
     this._domElem.css('background-color', 'yellow');
     this._domElem.css('width', `${width}px`);
     this._domElem.css('height', `${height}px`);
     this._domElem.css('position', 'absolute');
     this._domElem.css('left', `${x}px`);
-    this._domElem.css('top', `${y}px`);
+    this._domElem.css('top', `${y}px`);*/
   }
 
   onTagCreation(tuioTag) {
     super.onTagCreation(tuioTag);
+    console.log('creation', + JSON.stringify(tuioTag));
     if (this.isTouched(tuioTag.x, tuioTag.y)){
       this._domElem.css('background-color', players_color[tuioTag.id]);
     }
   }
 
   onTagDeletion(tuioTagId){
-    super.onTagDeletion(tuioTagId);
+      console.log('deletion', + JSON.stringify(tuioTag));
+
+      super.onTagDeletion(tuioTagId);
     this._domElem.css('background-color', 'yellow');
   }
 }
